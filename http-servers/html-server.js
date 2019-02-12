35b7f5a4325d5b307bd4920aae946138531a7fc2
Serhii <Serhii_Udovenko@epam.com>
2019-02-12 13:31:59 +0200

    replace read sync by readStream

--- http-servers/html-server.js
+++ http-servers/html-server.js
@@ -7,9 +7,24 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  const htmlFile = fs.readFileSync('./files/index.html', 'utf8');
-  const htmlWithMessage = htmlFile.replace('{message}', 'Message from server');
 
-  res.end(htmlWithMessage)
+
+//   const htmlFile = fs.readFileSync('./files/index.html', 'utf8');
+//   const htmlWithMessage = htmlFile.replace('{message}', 'Message from server');
+	const htmlFileStream = fs.createReadStream('./files/index.html');
+	let htmlWithMessage;
+	htmlFileStream.on('readable', () => {
+		const data = htmlFileStream.read();
+		if (data !== null && data.toString().indexOf('{message}' > -1)) {
+			htmlWithMessage = data.toString().replace('{message}', 'Message from server')
+		}
+	})
+	htmlFileStream.on('end', () => {
+		console.log("stream ended")
+		res.end(htmlWithMessage)
+	})
+	
+
+  
 });
 server.listen(port, () => console.log(`Listening: localhost:${port}`));
