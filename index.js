a8be82859026c35f7247c88449ee22bdf165b3f8
Sergey Udovenko <sergey.udovenko@p2h.com>
2019-02-12 08:11:08 +0200

    hw4

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(queryParsingMiddleware);
 
+app.use('/', routers);
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`App listening on port ${port}`))
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,49 @@
 const router = require('express').Router();
+const usersData = require('../data/users');
+const productsData = require('../data/products');
+const users = usersData.users || [];
+const products = productsData.products || [];
 
-router.route('/api/users').get();
+router.route('/api/users').get((req, res, next) => {
+  res.json(users);
+  next();
+});
 
 router.route('/api/products')
-  .get()
-  .post();
+  .get((req, res, next) => {
+    res.json(products);
+    next();
+  })
+  .post((req, res, next) => {
+    const product = req.body;
+    products.push(product);
+    res.json(product);
+    next();
+  });
 
-router.route('/api/products/:id').get();
+router.route('/api/products/:id').get((req, res, next) => {
+  const productId = req.params.id
+  const product = products.find((product) => product.id === +productId);
+    try {
+      res.json(product);
+    }
+    catch(err) {
+      console.log(err)
+    }
+    next();
+  });
 
-router.route('/api/products/:id/reviews').get();
+router.route('/api/products/:id/reviews').get((req, res, next) => {
+  const productId = req.params.id
+  const product = products.find((product) => product.id === +productId);
+  try {
+    const reviews = product.reviews || [];
+    res.json(reviews);
+  }
+  catch(err) {
+    console.log(err)
+  }
+  next();
+});
 
 module.exports = router
