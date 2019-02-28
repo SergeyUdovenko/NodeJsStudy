140279065aed7c34349c822fed33827f8ecd3bef
Serhii <Serhii_Udovenko@epam.com>
2019-02-28 16:35:15 +0200

    refactored

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -22,6 +22,16 @@ router.route('/local')
 router.route('/facebook').post(passport.authenticate('facebook'),
   Controllers.auth.login
 )
+router.get('/facebook',
+	passport.authenticate('facebook'));
+
+router.get('/facebook/callback',
+	Controllers.auth.login,
+	function (req, res) {
+		// Successful authentication, redirect home.
+		res.redirect('/');
+	});
+	
 router.route('/google').post(passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
   Controllers.auth.login
 )
--- strategies/facebook.strategy.js
+++ strategies/facebook.strategy.js
@@ -4,7 +4,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
     clientID: 1199797633512567,
     clientSecret: "47fccea4f262f69fb6521ab5ed1148d2",
-    callbackURL: "/auth/local"
+    callbackURL: "/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
