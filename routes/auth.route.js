fb8b88995127a2191760e2a06a4033bdb1b251b1
Sergey Udovenko <sergey.udovenko@p2h.com>
2019-03-07 08:54:42 +0200

    refactored

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -23,16 +23,28 @@ router.route('/facebook').post(passport.authenticate('facebook'),
   Controllers.auth.login
 )
 router.get('/facebook',
-	passport.authenticate('facebook'));
+  passport.authenticate('facebook'));
 
 router.get('/facebook/callback',
-	Controllers.auth.login,
-	function (req, res) {
-		// Successful authentication, redirect home.
-		res.redirect('/');
-	});
-	
-router.route('/google').post(passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
+  Controllers.auth.login,
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+router.route('/google').post(passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login']
+  }),
   Controllers.auth.login
 )
+
+router.get('/google',
+  passport.authenticate('google'));
+
+router.get('/google/callback',
+  Controllers.auth.login,
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 module.exports = router;
