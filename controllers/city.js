06c3c9ab77c0bcb2b361731b185dc953700c7c59
Serhii <Serhii_Udovenko@epam.com>
2019-03-21 17:04:42 +0200

    123

--- controllers/city.js
+++ controllers/city.js
@@ -6,79 +6,88 @@ const utils = require('../utils');
 const CityModel = require('../models').CityModel;
 
 module.exports.getRandom = (req, res) => {
-  MongoClient.connect(config.mongoCongif.url, function (error, client) {
-    assert.equal(null, error);
-    console.log('We are connected... from controller')
+	MongoClient.connect(config.mongoCongif.url, function (error, client) {
+		assert.equal(null, error);
+		console.log('We are connected... from controller')
 
-    const db = client.db(config.mongoCongif.dbName);
-    const collection = db.collection('cities');
-	console.log(db.collection('cities'));
-    const cities = collection
-      .find()
-      .limit(-1)
-      .skip(utils.randomizer());
+		const db = client.db(config.mongoCongif.dbName);
+		const collection = db.collection('cities');
+		console.log(db.collection('cities'));
+		const cities = collection
+			.find()
+			.limit(-1)
+			.skip(utils.randomizer());
 
-    cities.toArray((error, data)=>{
-      assert.equal(null, error);
-      res.json(data)
-    });
+		cities.toArray((error, data) => {
+			assert.equal(null, error);
+			res.json(data)
+		});
 
-    client.close();
-  });
+		client.close();
+	});
 }
 
 module.exports.getAll = (req, res) => {
 
-  CityModel.find((error, data) => {
-    if (error) {
-      console.log('Error in `getAllCities` controller');
-      return;
-    }
+	CityModel.find((error, data) => {
+		if (error) {
+			console.log('Error in `getAllCities` controller');
+			return;
+		}
 
-    res.json(data)
-  })
+		res.json(data)
+	})
 };
+
+
+
 module.exports.update = (req, res) => {
 	const id = req.params.id;
-	CityModel.findByIdAndUpdate({_id: id}, req.body, {new: true}).exec((error)=>{
+	CityModel.findOneAndUpdate({
+		_id: id
+	}, req.body, {
+		new: true
+	}).exec((error) => {
 		if (error) {
-		  res.status(500).end('Error: fail to find City with id: ', id)
+			res.status(500).end('Error: fail to find City with id: ', id)
 		}
 		res.end(`Success: City with id: ${id} updated`)
-	  })
+	})
 }
 module.exports.create = (req, res) => {
 
-  const user = req.body;
+	const user = req.body;
 
-  CityModel.create(user, (error, data) => {
-    if (error) {
-      res.status(500).end('Error: fail to create City: ')
-    }
+	CityModel.create(user, (error, data) => {
+		if (error) {
+			res.status(500).end('Error: fail to create City: ')
+		}
 
-    res.json(user)
-  })
+		res.json(user)
+	})
 };
 
 module.exports.delete = (req, res) => {
-  const id = req.params.id;
+	const id = req.params.id;
 
-  CityModel.findOneAndRemove({_id: id}).exec((error)=>{
-    if (error) {
-      res.status(500).end('Error: fail to find City with id: ', id)
-    }
-    res.end(`Success: City with id: ${id} removed`)
-  })
+	CityModel.findOneAndRemove({
+		_id: id
+	}).exec((error) => {
+		if (error) {
+			res.status(500).end('Error: fail to find City with id: ', id)
+		}
+		res.end(`Success: City with id: ${id} removed`)
+	})
 };
 
 module.exports.init = (req, res) => {
 
-  const cities = require('../data/cities');
+	const cities = require('../data/cities');
 
-  CityModel.create(cities, (error, data) =>{
-    if (error) console.log(error);
+	CityModel.create(cities, (error, data) => {
+		if (error) console.log(error);
 
-    res.json(data)
-  })
+		res.json(data)
+	})
 
 };
--- models/city.js
+++ models/city.js
@@ -2,12 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const City = new Schema({
-  name: String,
-  country: {type: String, default: ''},
-  capital: {type: Boolean, default: false},
-  location: Object
+	name: String,
+	country: {
+		type: String,
+		default: ''
+	},
+	capital: {
+		type: Boolean,
+		default: false
+	},
+	location: Object,
+	lastModifiedDate: Date,
+});
+
+City.pre('save', function (doc, next) {
+	this.lastModifiedDate = Date.now();
+	next();
+});
+
+City.pre('update', function (doc,next) {
+	this.lastModifiedDate = Date.now();
+	next();
 });
 
 module.exports = {
-  CityModel: mongoose.model('City', City)
+	CityModel: mongoose.model('City', City)
 };
